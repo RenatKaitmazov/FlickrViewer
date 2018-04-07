bfb34bdfdc60c67a2b669d3743ee207c2a9cfe7e
Renat Kaitmazov <renatkaitmazov@gmail.com>
2018-04-07 08:31:33 +0300

    Initial commit

---
+++ app/src/main/java/lz/renatkaitmazov/flickrviewer/PhotoListActivity.kt
@@ -0,0 +1,12 @@
+package lz.renatkaitmazov.flickrviewer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_list)
+    }
+}
